dde44080c38976a79202281397ab803d27860aad
nvds <1052934+nvdsteen@users.noreply.github.com>
2024-10-03 09:17:00 +0200

    [UPD] remove secondary sensors

--- psa_xml.py
+++ psa_xml.py
@@ -27,7 +27,6 @@ def main():
         root = tree.getroot()
 
         # Initialize a list to store data for the dataframe
-        data = []
 
         # Track all parsed tags for completeness check
         parsed_tags = set()
@@ -65,222 +64,220 @@ def main():
                 ),
 
             }
+        for calc_array_tag in [el.tag for el in root if el.tag.endswith("CalcArray")]:
+            data = []
 
-        # Parse CalcArrayItems
-        calc_array = tree.find("CalcArray")
-        calc_array_tag = "CalcArray"
-        if calc_array is None:
-            calc_array = tree.find("AverageCalcArray")
-            calc_array_tag = "AverageCalcArray"
+            # Parse CalcArrayItems
+            calc_array = tree.find(calc_array_tag)
 
-        for item in calc_array.findall("CalcArrayItem"):
-            data.append(extract_calc_item(item))
+            for item in calc_array.findall("CalcArrayItem"):
+                data.append(extract_calc_item(item))
 
-        calc_items = data[:]
+            calc_items = data[:]
 
-        # Parse other single value elements
-        single_value_elements = [
-            "Version",
-            "ServerName",
-            "InstrumentPath",
-            "InstrumentMatch",
-            "InputDir",
-            "OutputDir",
-            "NameAppend",
-            "OutputFile",
-            "LastProcessed",
-            "ProcessScansToEnd",
-            "ScansToSkip",
-            "ScansToProcess",
-            "MergeHeaderFile",
-            "OutputFormat",
-            "FromCast",
-            "CreateFile",
-            "ScanRangeSource",
-            "ScanRangeOffset",
-            "ScanRangeDuration",
-            "StartTimeOption",
-            "PromptForNoteAndOrStartTime",
-        ]
+            # Parse other single value elements
+            single_value_elements = [
+                "Version",
+                "ServerName",
+                "InstrumentPath",
+                "InstrumentMatch",
+                "InputDir",
+                "OutputDir",
+                "NameAppend",
+                "OutputFile",
+                "LastProcessed",
+                "ProcessScansToEnd",
+                "ScansToSkip",
+                "ScansToProcess",
+                "MergeHeaderFile",
+                "OutputFormat",
+                "FromCast",
+                "CreateFile",
+                "ScanRangeSource",
+                "ScanRangeOffset",
+                "ScanRangeDuration",
+                "StartTimeOption",
+                "PromptForNoteAndOrStartTime",
+            ]
 
-        for elem in single_value_elements:
-            el = tree.find(elem)
-            if el is not None:
-                data.append(
-                    {"section": "SingleElement", "name": elem, "value": el.get("value")}
-                )
-                parsed_tags.add(elem)
+            for elem in single_value_elements:
+                el = tree.find(elem)
+                if el is not None:
+                    data.append(
+                        {"section": "SingleElement", "name": elem, "value": el.get("value")}
+                    )
+                    parsed_tags.add(elem)
 
-        # Parse MiscellaneousDataForCalculations section
-        misc_section = tree.find("MiscellaneousDataForCalculations")
-        if misc_section is not None:
-            parsed_tags.add("MiscellaneousDataForCalculations")
-            misc_data = {
-                "section": "MiscellaneousDataForCalculations",
-                "Latitude": misc_section.find("Latitude").get("value"),
-                "Longitude": misc_section.find("Longitude").get("value"),
+            # Parse MiscellaneousDataForCalculations section
+            misc_section = tree.find("MiscellaneousDataForCalculations")
+            if misc_section is not None:
+                parsed_tags.add("MiscellaneousDataForCalculations")
+                misc_data = {
+                    "section": "MiscellaneousDataForCalculations",
+                    "Latitude": misc_section.find("Latitude").get("value"),
+                    "Longitude": misc_section.find("Longitude").get("value"),
+                }
+                descent_rate = misc_section.find("DescentRateAndAcceleration")
+                if descent_rate is not None:
+                    misc_data["DescentRate_WindowSize"] = descent_rate.find("WindowSize").get(
+                        "value"
+                    )
+
+                oxygen = misc_section.find("Oxygen")
+                if oxygen is not None:
+                    misc_data["Oxygen_WindowSize"] = oxygen.find("WindowSize").get("value")
+                    misc_data["ApplyHysteresisCorrection"] = oxygen.find(
+                        "ApplyHysteresisCorrection"
+                    ).get("value")
+                    misc_data["ApplyTauCorrection"] = oxygen.find("ApplyTauCorrection").get(
+                        "value"
+                    )
+
+                avg_sound_velocity = misc_section.find("AverageSoundVelocity")
+                if avg_sound_velocity is not None:
+                    misc_data["AverageSoundVelocity_MinimumPressure"] = avg_sound_velocity.find(
+                        "MinimumPressure"
+                    ).get("value")
+                    misc_data["AverageSoundVelocity_MinimumSalinity"] = avg_sound_velocity.find(
+                        "MinimumSalinity"
+                    ).get("value")
+                    misc_data["AverageSoundVelocity_PressureWindowSize"] = (
+                        avg_sound_velocity.find("PressureWindowSize").get("value")
+                    )
+                    misc_data["AverageSoundVelocity_TimeWindowSize"] = avg_sound_velocity.find(
+                        "TimeWindowSize"
+                    ).get("value")
+
+                data.append(misc_data)
+
+            # Parse InputFileArray
+            input_file_array = tree.find("InputFileArray")
+            if input_file_array is not None:
+                parsed_tags.add("InputFileArray")
+                array_items = input_file_array.findall("ArrayItem")
+                for array_item in array_items:
+                    data.append(
+                        {
+                            "section": "InputFileArray",
+                            "index": array_item.get("index"),
+                            "value": array_item.get("value"),
+                        }
+                    )
+
+            # Convert the data list to a pandas DataFrame
+            df = pd.DataFrame(data)
+
+            # List of all expected tags in the XML (adjust this according to your actual XML structure)
+            expected_tags = {
+                "Version",
+                "ServerName",
+                "InstrumentPath",
+                "InstrumentMatch",
+                "InputDir",
+                "OutputDir",
+                "NameAppend",
+                "OutputFile",
+                "LastProcessed",
+                "ProcessScansToEnd",
+                "ScansToSkip",
+                "ScansToProcess",
+                "MergeHeaderFile",
+                "OutputFormat",
+                "FromCast",
+                "CreateFile",
+                "ScanRangeSource",
+                "ScanRangeOffset",
+                "ScanRangeDuration",
+                "StartTimeOption",
+                "PromptForNoteAndOrStartTime",
+                "CalcArrayItem",
+                "MiscellaneousDataForCalculations",
+                "InputFileArray",
             }
-            descent_rate = misc_section.find("DescentRateAndAcceleration")
-            if descent_rate is not None:
-                misc_data["DescentRate_WindowSize"] = descent_rate.find("WindowSize").get(
-                    "value"
-                )
 
-            oxygen = misc_section.find("Oxygen")
-            if oxygen is not None:
-                misc_data["Oxygen_WindowSize"] = oxygen.find("WindowSize").get("value")
-                misc_data["ApplyHysteresisCorrection"] = oxygen.find(
-                    "ApplyHysteresisCorrection"
-                ).get("value")
-                misc_data["ApplyTauCorrection"] = oxygen.find("ApplyTauCorrection").get(
-                    "value"
-                )
+            # Issue a warning if not everything is parsed
+            missing_tags = expected_tags - parsed_tags
+            # if missing_tags:
+                # warnings.warn(
+                    # f"The following tags were not parsed into the DataFrame: {', '.join(missing_tags)}"
+                # )
 
-            avg_sound_velocity = misc_section.find("AverageSoundVelocity")
-            if avg_sound_velocity is not None:
-                misc_data["AverageSoundVelocity_MinimumPressure"] = avg_sound_velocity.find(
-                    "MinimumPressure"
-                ).get("value")
-                misc_data["AverageSoundVelocity_MinimumSalinity"] = avg_sound_velocity.find(
-                    "MinimumSalinity"
-                ).get("value")
-                misc_data["AverageSoundVelocity_PressureWindowSize"] = (
-                    avg_sound_velocity.find("PressureWindowSize").get("value")
-                )
-                misc_data["AverageSoundVelocity_TimeWindowSize"] = avg_sound_velocity.find(
-                    "TimeWindowSize"
-                ).get("value")
+            def delete_none(_dict):
+                """Delete None values recursively from all of the dictionaries, tuples, lists, sets"""
+                if isinstance(_dict, dict):
+                    for key, value in list(_dict.items()):
+                        if isinstance(value, (list, dict, tuple, set)):
+                            _dict[key] = delete_none(value)
+                        elif value is None or key is None:
+                            del _dict[key]
 
-            data.append(misc_data)
+                elif isinstance(_dict, (list, set, tuple)):
+                    _dict = type(_dict)(delete_none(item) for item in _dict if item is not None)
 
-        # Parse InputFileArray
-        input_file_array = tree.find("InputFileArray")
-        if input_file_array is not None:
-            parsed_tags.add("InputFileArray")
-            array_items = input_file_array.findall("ArrayItem")
-            for array_item in array_items:
-                data.append(
-                    {
-                        "section": "InputFileArray",
-                        "index": array_item.get("index"),
-                        "value": array_item.get("value"),
-                    }
-                )
+                return _dict
 
-        # Convert the data list to a pandas DataFrame
-        df = pd.DataFrame(data)
+            filtered_dict = delete_none(df.replace({np.nan: None}).to_dict(orient="records"))
+            with open("/tmp/output.yaml", "w") as file:
+                yaml.dump(filtered_dict, file, default_flow_style=False)
 
-        # List of all expected tags in the XML (adjust this according to your actual XML structure)
-        expected_tags = {
-            "Version",
-            "ServerName",
-            "InstrumentPath",
-            "InstrumentMatch",
-            "InputDir",
-            "OutputDir",
-            "NameAppend",
-            "OutputFile",
-            "LastProcessed",
-            "ProcessScansToEnd",
-            "ScansToSkip",
-            "ScansToProcess",
-            "MergeHeaderFile",
-            "OutputFormat",
-            "FromCast",
-            "CreateFile",
-            "ScanRangeSource",
-            "ScanRangeOffset",
-            "ScanRangeDuration",
-            "StartTimeOption",
-            "PromptForNoteAndOrStartTime",
-            "CalcArrayItem",
-            "MiscellaneousDataForCalculations",
-            "InputFileArray",
-        }
+            df_calc = pd.DataFrame(calc_items)
+            df_calc = df_calc.set_index("index")
+            df_calc_reset = df_calc.drop(
+                df_calc[df_calc["FullName"].str.contains(", 2 ")].index
+            ).reset_index(drop=True)
+            # df_calc_reset = df_calc.reset_index(drop=True)
 
-        # Issue a warning if not everything is parsed
-        missing_tags = expected_tags - parsed_tags
-        # if missing_tags:
-            # warnings.warn(
-                # f"The following tags were not parsed into the DataFrame: {', '.join(missing_tags)}"
-            # )
+            # Create the root XML element
+            calcarray_root = ET.Element(calc_array_tag, attrib={"Size": str(df_calc_reset.shape[0])})
 
-        def delete_none(_dict):
-            """Delete None values recursively from all of the dictionaries, tuples, lists, sets"""
-            if isinstance(_dict, dict):
-                for key, value in list(_dict.items()):
-                    if isinstance(value, (list, dict, tuple, set)):
-                        _dict[key] = delete_none(value)
-                    elif value is None or key is None:
-                        del _dict[key]
+            # Columns that should be used as attributes of the 'Calc' element
+            calc_attributes = ["UnitID", "Ordinal"]
 
-            elif isinstance(_dict, (list, set, tuple)):
-                _dict = type(_dict)(delete_none(item) for item in _dict if item is not None)
+            # Iterate through DataFrame rows and create XML elements
+            for index, row in df_calc_reset.iterrows():
+                # Create a CalcArrayItem element
+                calc_item = ET.SubElement(calcarray_root, row["section"], attrib={"index": str(index), "CalcID": row["CalcID"]})
 
-            return _dict
+                # Add child Calc element
+                calc_attrs = {
+                    attr: str(row[attr]) for attr in calc_attributes if pd.notna(row[attr])
+                }
+                calc = ET.SubElement(calc_item, "Calc", calc_attrs)
 
-        filtered_dict = delete_none(df.replace({np.nan: None}).to_dict(orient="records"))
-        with open("/tmp/output.yaml", "w") as file:
-            yaml.dump(filtered_dict, file, default_flow_style=False)
+                # Add other columns dynamically
+                for col in df_calc_reset.columns:
+                    if col not in ["section", "CalcID"] + calc_attributes and pd.notna(row[col]):
+                        # Create an element with column name as tag, value as attribute
+                        child = ET.SubElement(calc, col)
+                        child.set("value", str(row[col]))
 
-        df_calc = pd.DataFrame(calc_items)
-        df_calc = df_calc.set_index("index")
-        df_calc_reset = df_calc.drop(
-            df_calc[df_calc["FullName"].str.contains(", 2 ")].index
-        ).reset_index(drop=True)
-        # df_calc_reset = df_calc.reset_index(drop=True)
+                # Delete old calcarray
+            calc_array_index = None
+            for i, child in enumerate(root):
+                if child.tag == calc_array_tag:
+                    calc_array_index = i
+                    root.remove(child)
+                    break
 
-        # Create the root XML element
-        calcarray_root = ET.Element(calc_array_tag, attrib={"Size": str(df_calc_reset.shape[0])})
+            if calc_array_index is None:
+                calc_array_index = len(root)
 
-        # Columns that should be used as attributes of the 'Calc' element
-        calc_attributes = ["UnitID", "Ordinal"]
+            root.insert(calc_array_index, calcarray_root)
+            # Create new elements for the top and bottom lines
+            # top_line = ET.Element('TopLine')  # You can set any tag and attributes you want
+            # top_line.text = '{{# data}}'
 
-        # Iterate through DataFrame rows and create XML elements
-        for index, row in df_calc_reset.iterrows():
-            # Create a CalcArrayItem element
-            calc_item = ET.SubElement(calcarray_root, row["section"], attrib={"index": str(index), "CalcID": row["CalcID"]})
+            # bottom_line = ET.Element('BottomLine')  # Similarly for the bottom line
+            # bottom_line.text = '{{/ data}}'
 
-            # Add child Calc element
-            calc_attrs = {
-                attr: str(row[attr]) for attr in calc_attributes if pd.notna(row[attr])
-            }
-            calc = ET.SubElement(calc_item, "Calc", calc_attrs)
-
-            # Add other columns dynamically
-            for col in df_calc_reset.columns:
-                if col not in ["section", "CalcID"] + calc_attributes and pd.notna(row[col]):
-                    # Create an element with column name as tag, value as attribute
-                    child = ET.SubElement(calc, col)
-                    child.set("value", str(row[col]))
-
-            # Delete old calcarray
-        calc_array_index = None
-        for i, child in enumerate(root):
-            if child.tag == calc_array_tag:
-                calc_array_index = i
-                root.remove(child)
-                break
-
-        if calc_array_index is None:
-            calc_array_index = len(root)
-
-        root.insert(calc_array_index, calcarray_root)
-        # Create new elements for the top and bottom lines
-        # top_line = ET.Element('TopLine')  # You can set any tag and attributes you want
-        # top_line.text = '{{# data}}'
-
-        # bottom_line = ET.Element('BottomLine')  # Similarly for the bottom line
-        # bottom_line.text = '{{/ data}}'
-
-        # root.insert(0, top_line)
-        # root.append(bottom_line)
+            # root.insert(0, top_line)
+            # root.append(bottom_line)
         ET.indent(root)
         pretty_xml_str = ET.tostring(
             root, encoding="unicode", method="xml", xml_declaration=True
         )
         pretty_xml_str = "{{# data}}\n" + pretty_xml_str + "\n\n{{/ data}}\n\n"
-        file_out = Path("./psa_templates/only_primary").joinpath(Path(psa_file).name)
+        file_out = Path("./psa_templates").joinpath(Path(psa_file).name)
         with open(file_out, mode="w", encoding="utf-8") as f:
             f.write(pretty_xml_str)
 
