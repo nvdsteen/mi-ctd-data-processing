e838255f090dbdaae6392214d2bcf78d6e4e419a
nvandensteen <1052934+nvdsteen@users.noreply.github.com>
2024-11-05 11:42:34 +0100

    [UPD] remove content main (openpyxl dependency)

--- scripts/filename_matching.py
+++ scripts/filename_matching.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import pandas as pd
-import openpyxl
+# import openpyxl
 import re
 
 
@@ -62,71 +62,71 @@ def adjust_worksheet_columns_width(worksheet):
 
 
 def main():
-    path_elements = ["Documents", "RBINS", "Onedrive", "CTD_testing"]
-    folder = Path().home().joinpath(*path_elements)
-    out = Path().home().joinpath(*path_elements).joinpath("mismatched_filenames")
-    if not out.exists():
-        out.mkdir()
+    # path_elements = ["Documents", "RBINS", "Onedrive", "CTD_testing"]
+    # folder = Path().home().joinpath(*path_elements)
+    # out = Path().home().joinpath(*path_elements).joinpath("mismatched_filenames")
+    # if not out.exists():
+    #     out.mkdir()
 
-    raw_folders = folder.rglob("raw_files")
-    out_overview = out.parent.joinpath("overview.xlsx")
+    # raw_folders = folder.rglob("raw_files")
+    # out_overview = out.parent.joinpath("overview.xlsx")
 
-    columns = [".xmlcon", ".hex", ".bl", ".mrk", ".hdr", ".btl", ".cnv"]
-    skip_columns = [".cnv", ".btl"]
+    # columns = [".xmlcon", ".hex", ".bl", ".mrk", ".hdr", ".btl", ".cnv"]
+    # skip_columns = [".cnv", ".btl"]
 
-    columns = [ci for ci in columns if ci not in skip_columns]
+    # columns = [ci for ci in columns if ci not in skip_columns]
 
-    content_overview = []
-    for di in raw_folders:
-        di_name = di.parent.name
-        dfi = get_df_files(di)
+    # content_overview = []
+    # for di in raw_folders:
+    #     di_name = di.parent.name
+    #     dfi = get_df_files(di)
 
-        relevant_columns = sorted(
-            list(set(dfi.columns).intersection(columns)), key=lambda i: columns.index(i)
-        )
-        additional_columns = set(dfi.columns).difference(relevant_columns)
+    #     relevant_columns = sorted(
+    #         list(set(dfi.columns).intersection(columns)), key=lambda i: columns.index(i)
+    #     )
+    #     additional_columns = set(dfi.columns).difference(relevant_columns)
 
-        output_file = out.joinpath(di_name + "_mismatching_files.xlsx")
-        rows_with_mismatch = dfi[relevant_columns].isna().any(axis=1)
-        dfi_mismatch = dfi.loc[rows_with_mismatch, relevant_columns]
-        dfi_mismatch = dfi_mismatch.dropna(axis=0, how="all")
-        btl_in_folder = ".btl" in relevant_columns
+    #     output_file = out.joinpath(di_name + "_mismatching_files.xlsx")
+    #     rows_with_mismatch = dfi[relevant_columns].isna().any(axis=1)
+    #     dfi_mismatch = dfi.loc[rows_with_mismatch, relevant_columns]
+    #     dfi_mismatch = dfi_mismatch.dropna(axis=0, how="all")
+    #     btl_in_folder = ".btl" in relevant_columns
 
-        if rows_with_mismatch.any() or btl_in_folder:
-            sheet_name = "Mismatching filenames"
-            with pd.ExcelWriter(output_file, engine="openpyxl") as writer:
-                dfi_mismatch.to_excel(
-                    writer, sheet_name=sheet_name, startrow=3, index=False
-                )
+    #     if rows_with_mismatch.any() or btl_in_folder:
+    #         sheet_name = "Mismatching filenames"
+    #         with pd.ExcelWriter(output_file, engine="openpyxl") as writer:
+    #             dfi_mismatch.to_excel(
+    #                 writer, sheet_name=sheet_name, startrow=3, index=False
+    #             )
 
-            wb = openpyxl.load_workbook(output_file)
-            ws = wb[sheet_name]
-            ws["A1"] = f"Addition columns:"
-            ws["B1"] = f"{', '.join(additional_columns)}"
-            ws["A2"] = f".btl files in raw_files folder:"
-            ws["B2"] = f"{'.btl' in relevant_columns}"
+    #         wb = openpyxl.load_workbook(output_file)
+    #         ws = wb[sheet_name]
+    #         ws["A1"] = f"Addition columns:"
+    #         ws["B1"] = f"{', '.join(additional_columns)}"
+    #         ws["A2"] = f".btl files in raw_files folder:"
+    #         ws["B2"] = f"{'.btl' in relevant_columns}"
 
-            adjust_worksheet_columns_width(worksheet=ws)
-            wb.save(output_file)
+    #         adjust_worksheet_columns_width(worksheet=ws)
+    #         wb.save(output_file)
 
-            content_overview += [
-                (
-                    di_name,
-                    f'=HYPERLINK("{output_file.relative_to(out_overview.parent).as_posix()}", "{di_name}"',
-                )
-            ]
-        else:
-            content_overview += [(di_name, "")]
-    df_overview = pd.DataFrame(
-        content_overview, columns=["Dataset", "Link"]
-    ).sort_values("Dataset")
-    sheet_name_overview = "Filename mismatching datasets"
-    with pd.ExcelWriter(out_overview, engine="openpyxl") as writer:
-        df_overview.to_excel(writer, sheet_name=sheet_name_overview, index=False)
-    wb = openpyxl.load_workbook(out_overview)
-    ws = wb[sheet_name_overview]
-    adjust_worksheet_columns_width(worksheet=ws)
-    wb.save(out_overview)
+    #         content_overview += [
+    #             (
+    #                 di_name,
+    #                 f'=HYPERLINK("{output_file.relative_to(out_overview.parent).as_posix()}", "{di_name}"',
+    #             )
+    #         ]
+    #     else:
+    #         content_overview += [(di_name, "")]
+    # df_overview = pd.DataFrame(
+    #     content_overview, columns=["Dataset", "Link"]
+    # ).sort_values("Dataset")
+    # sheet_name_overview = "Filename mismatching datasets"
+    # with pd.ExcelWriter(out_overview, engine="openpyxl") as writer:
+    #     df_overview.to_excel(writer, sheet_name=sheet_name_overview, index=False)
+    # wb = openpyxl.load_workbook(out_overview)
+    # ws = wb[sheet_name_overview]
+    # adjust_worksheet_columns_width(worksheet=ws)
+    # wb.save(out_overview)
 
 
 if __name__ == "__main__":
