8a2e07f8ce8fbe0ed0d1be1cba44875547e78a52
nvandensteen <1052934+nvdsteen@users.noreply.github.com>
2025-01-09 16:43:59 +0100

    [FIX] bin_df/bin_data when column is all nan

--- scripts/seabird_processes.py
+++ scripts/seabird_processes.py
@@ -119,6 +119,7 @@ def bin_data(input_df, cast, zcord, profile_id, params_out, bin_width=1.):
     bin_df = bin_df.drop(columns=[zcord]).rename(columns={'bin': zcord})
     
     bin_df = bin_df[params_out].rename(columns={'bin': zcord})
+    bin_df = bin_df.dropna(axis=1, how='all')
     bin_df = bin_df.dropna()
     return bin_df
 
