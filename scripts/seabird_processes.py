554f3d391bac3f99855c3091d0837f40e9738774
nvandensteen <1052934+nvdsteen@users.noreply.github.com>
2025-01-13 15:52:36 +0100

    [FIX] updated dropna bin_df

--- scripts/seabird_processes.py
+++ scripts/seabird_processes.py
@@ -119,6 +119,6 @@ def bin_data(input_df, cast, zcord, profile_id, params_out, bin_width=1.):
     bin_df = bin_df.drop(columns=[zcord]).rename(columns={'bin': zcord})
     
     bin_df = bin_df[params_out].rename(columns={'bin': zcord})
-    bin_df = bin_df.dropna(subset=bin_df.columns.difference(["CTD_number, depSM"]), how="all")
+    bin_df = bin_df.dropna(subset=bin_df.columns.difference(["CTD number", "depSM"]), how="all")
     return bin_df
 
